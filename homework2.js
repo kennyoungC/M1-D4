f3630168f14def54fb2bb68de91088c16197667e
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-02-18 00:37:30 +0200

    commit
    update on my homework

--- homework2.js
+++ homework2.js
@@ -16,8 +16,8 @@ es:
 */
 
 let array = [1, 2, 3, 4, 5, 6, 7];
-// array.reverse();
-// console.log(array);
+array.reverse();
+console.log(array);
 
 console.log("\n--------------< EXERCISE 2 >--------------");
 
@@ -25,14 +25,14 @@ console.log("\n--------------< EXERCISE 2 >--------------");
 Write the code to get the maximum value in an array.
 */
 
-// console.log(Math.max(...array));
+console.log(Math.max(...array));
 
 console.log("\n--------------< EXERCISE 3 >--------------");
 /* EXERCISE 3
 Write the code to get the minimum value in an array.
 */
 
-// console.log(Math.min(...array));
+console.log(Math.min(...array));
 
 console.log("\n--------------< EXERCISE 4 >--------------");
 
@@ -40,44 +40,65 @@ console.log("\n--------------< EXERCISE 4 >--------------");
 Write the code to get only even numerical values in an array.
 */
 
-for (i = 0; i < array.length; i++) {
+for (let i = 0; i < array.length; i++) {
+  // console.log(i, array[i]);
   if (array[i] % 2 === 0) {
     console.log(array[i]);
-  } else {
   }
-  continue;
 }
 
-// array.splice(0, array.length);
-// console.log(array);
+console.log("\n--------------< EXERCISE 5 >--------------");
+
 /* EXERCISE 5
 Write the code to delete even entries from an array.
 */
-let evenNumber = [];
 for (let i = 0; i < array.length; i++) {
+  // console.log(i, array[i]);
   if (array[i] % 2 === 0) {
+    // console.log(array[i]);
+    array.splice(i, 1);
   }
-  evenNumber.push(array[i]);
-  console.log(evenNumber);
 }
+console.log(array);
+console.log("\n--------------< EXERCISE 6 >--------------");
+
 /* EXERCISE 6
 Write the code to remove all the vowels from a string.
 */
+let str = "house";
+let noVowels = str.replace(/[aeiou]/gi, "");
+// for (let i = 0; i < str.length; i++)
+// if (
+//   str[i] === "a" ||
+//   str[i] === "e" ||
+//   str[i] === "i" ||
+//   str[i] === "o" ||
+//   str[i] === "u"
+// )
+console.log(noVowels);
 
-/* WRITE YOUR CODE HERE */
+console.log("\n--------------< EXERCISE 7 >--------------");
 
 /* EXERCISE 7
 Write the code to increase all the numeric values in a array by 1.
 */
 
-/* WRITE YOUR CODE HERE */
+let numericValue = [4, 5, 6, 7, 3, 2];
+console.log(numericValue);
+for (let i = 0; i < numericValue.length; i++) console.log(numericValue[i] + 1);
+
+console.log("\n--------------< EXERCISE 8 >--------------");
 
 /* EXERCISE 8 
 Replace all the strings in an array with their length.
 es.: ["strive", "is", "great"] => [6, 2, 5]
 */
 
-/* WRITE YOUR CODE HERE */
+let str1 = ["strive", "is", "great"];
+str1[0] = 6;
+str1[1] = 2;
+str1[2] = 5;
+console.log(str1);
 
 /* WHEN YOU ARE FINISHED
 Send the code on the Eduflow platform. In the next days we'll also learn how to use GIT
